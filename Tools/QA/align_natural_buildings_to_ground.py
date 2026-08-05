0259bad5120f54252fdf9971df27c3b05814a6db
unknown <jefferson.amador@ucr.ac.cr>
2026-08-04 22:47:14 -0600

    level: finalize natural campus placement pass

---
+++ Tools/QA/align_natural_buildings_to_ground.py
@@ -0,0 +1,20 @@
+import unreal
+world = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_editor_world()
+aligned = 0
+for actor in unreal.EditorLevelLibrary.get_all_level_actors():
+    if not actor.get_actor_label().startswith('CampusBuilding_'):
+        continue
+    loc = actor.get_actor_location()
+    origin, extent = actor.get_actor_bounds(False)
+    start = unreal.Vector(loc.x, loc.y, 50000.0)
+    end = unreal.Vector(loc.x, loc.y, -50000.0)
+    try:
+        hit = unreal.SystemLibrary.line_trace_single(world, start, end, unreal.TraceTypeQuery.TRACE_TYPE_QUERY1, True, [actor], unreal.DrawDebugTrace.NONE)
+        if hit:
+            bottom_offset = loc.z - (origin.z - extent.z)
+            actor.set_actor_location(unreal.Vector(loc.x, loc.y, hit.location.z + bottom_offset), False, False)
+            aligned += 1
+    except Exception as e:
+        unreal.log_warning('Ground align ' + actor.get_actor_label() + ': ' + repr(e))
+unreal.EditorLevelLibrary.save_current_level()
+unreal.log('NATURAL_BUILDINGS_ALIGNED=' + str(aligned))
---
+++ Tools/QA/finalize_natural_campus_startup.py
@@ -0,0 +1,8 @@
+import unreal
+def run(path):
+    code = open(path, 'r', encoding='utf-8').read()
+    exec(compile(code, path, 'exec'), {})
+run('C:/Users/jeffa/Desktop/Proyecto/Proyecto-Memory/Tools/QA/place_natural_layout_markers.py')
+run('C:/Users/jeffa/Desktop/Proyecto/Proyecto-Memory/Tools/QA/complete_natural_campus_pass.py')
+run('C:/Users/jeffa/Desktop/Proyecto/Proyecto-Memory/Tools/QA/align_natural_buildings_to_ground.py')
+unreal.log('NATURAL_CAMPUS_FINAL_STARTUP_DONE')

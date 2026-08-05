cf5d8dadf3e98fec58dbb085e33e684419e9b1d1
unknown <jefferson.amador@ucr.ac.cr>
2026-08-04 23:38:59 -0600

    fix: raise natural campus player start

--- Tools/QA/ensure_natural_playable_setup.py
+++ Tools/QA/ensure_natural_playable_setup.py
@@ -4,9 +4,9 @@ actors = unreal.EditorLevelLibrary.get_all_level_actors()
 starts = [a for a in actors if isinstance(a, unreal.PlayerStart)]
 primary = next((a for a in starts if a.get_actor_label() == 'PlayerStart_Campus_Natural'), None)
 if primary is None:
-    primary = starts[0] if starts else unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.PlayerStart, unreal.Vector(-28000,-30000,650), unreal.Rotator(0,90,0))
+    primary = starts[0] if starts else unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.PlayerStart, unreal.Vector(-28000,-30000,3000), unreal.Rotator(0,90,0))
 primary.set_actor_label('PlayerStart_Campus_Natural')
-primary.set_actor_location(unreal.Vector(-28000,-30000,650), False, False)
+primary.set_actor_location(unreal.Vector(-28000,-30000,3000), False, False)
 primary.set_actor_rotation(unreal.Rotator(0,90,0), False)
 for start in starts:
     if start != primary:
